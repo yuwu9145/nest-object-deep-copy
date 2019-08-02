576a3eee5dacd3f19bd086aef111b7a0cbe59da1
yuchao wu <yuchao.sydneyuni@gmail.com>
2019-08-02 16:50:26 +1000

    Refactor code and also add tests again copy array

--- hard-copy.js
+++ hard-copy.js
@@ -1,62 +1,70 @@
-/**
+'use strict';
+
+/*****************
+ * HELPERS *
+ * **************/
+
+ /**
  * Dynamically sets a deeply nested value in an object.
- * Optionally "bores" a path to it if its undefined.
  * @function
  * @param {!object} obj  - The object which contains the value you want to change/set.
  * @param {!array} path  - The array representation of path to the value you want to change/set.
  * @param {!mixed} value - The value you want to set it to.
- * @param {boolean} setrecursively - If true, will set value of non-existing path as well.
  */
-function setDeep(obj, path, value, setrecursively = false) {
+function setDeep(obj, path, value) {
 
-    let level = 0;
+  var level = 0;
 
-    path.reduce((a, b)=>{
-        level++;
+  path.reduce((a, b)=>{
+    level++;
 
-        if (setrecursively && typeof a[b] === "undefined" && level !== path.length){
-            a[b] = {};
-            return a[b];
-        }
-
-        if (level === path.length){
-            a[b] = value;
-            return value;
-        } else {
-            return a[b];
-        }
-    }, obj);
-}
-
-function getDeep(path, obj) {
-  return path.reduce(function(prev, curr) {
-      return prev ? prev[curr] : null
-  }, obj || self)
-}
-
-let copiedObj = Object.assign({});
-
-const keyChain = [];
-let subKeyChain = [];
-
-function iterateKeys(obj) {
-  Object.keys(obj).forEach(key => {
-    const currentKeyChainValue = obj[key]; 
-    
-    if (typeof currentKeyChainValue === 'object' && obj[key] && obj[key] !== null) {
-      subKeyChain.push(key); 
-      iterateKeys(obj[key]);
-      return;
+    if (level === path.length){
+      a[b] = value;
+      return value;
+    } else {
+      return a[b];
     }
-    return;
-  });
+  }, obj);
 }
 
+/**
+ * Dynamically gets a deeply nested value from an object.
+ * @function
+ * @param {!array} path  - The array representation of path to the value you want to change/set.
+ * @param {!object} obj  - The object which contains the value you want to get.
+ */
+function getDeep(path, obj) {
+  return path.reduce(function(prev, curr) { return prev[curr] }, obj);
+}
+/*****************
+ * END OF HELPERS *
+ * **************/
 
+
+ /*****************
+ * MAIN CLOSURE *
+ * **************/
 function hardCopy(obj) {
+  
+  var copiedObj = Object.assign({});
+
+  var keyChain = [];
+  var subKeyChain = [];
+  
+  function iterateKeys(obj) {
+    Object.keys(obj).forEach(key => {
+      const currentKeyChainValue = obj[key]; 
+      if (typeof currentKeyChainValue === 'object' && obj[key] && obj[key] !== null) {
+        subKeyChain.push(key); 
+        iterateKeys(obj[key]);
+        return;
+      }
+      return;
+    });
+  }
+  
   Object.keys(obj).forEach(key => {
-    console.log(key); 
-    const currentKeyChainValue = obj[key]; 
+    var currentKeyChainValue = obj[key]; 
     if (typeof currentKeyChainValue === 'object' && obj[key] && obj[key] !== null) {
       subKeyChain = [...[key]];
       iterateKeys(obj[key]);
@@ -76,5 +84,8 @@ function hardCopy(obj) {
 
   return copiedObj;
 }
+ /*****************
+ * END OF MAIN CLOSURE *
+ * **************/
 
-module.exports = hardCopy;
+ module.exports = hardCopy;
--- hard-copy.test.js
+++ hard-copy.test.js
@@ -1,96 +1,180 @@
 const hardCopy = require('./hard-copy');
 
 describe('Plain Object', () => {
-  xit('should create a copied object which is a hard copied version', () => {
+	it('should create a copied object which is a hard copied version', () => {
+		const originalObj = {
+			a: undefined,
+			b: true,
+			c: false,
+			d: 'test',
+			e: 123,
+			f: null,
+			g: function() {
+				return 'I am a function from original obj';
+			},
+			h: [1, 2, 3]
+		};
 
-    const originalObj = {
-      a: undefined,
-      b: true,
-      c: false,
-      d: 'test',
-      e: 123,
-      f: null,
-      g: function() { return 'I am a function from original obj'; },
-      h: [1, 2, 3] 
-    };
+		const copiedObj = hardCopy(originalObj);
 
-    const copiedObj = hardCopy(originalObj);
+		copiedObj.a = 1;
+		copiedObj.b = false;
+		copiedObj.c = true;
+		copiedObj.d = 'test changed';
+		copiedObj.e = 1234;
+		copiedObj.f = 'not null';
+		copiedObj.g = function() {
+			return 'I am a function from copied obj';
+		};
+		copiedObj.h = [1, 2, 3, 4];
 
-    copiedObj.a = 1;
-    copiedObj.b = false;
-    copiedObj.c = true;
-    copiedObj.d = 'test changed';
-    copiedObj.e = 1234;
-    copiedObj.f = 'not null';
-    copiedObj.g = function() { return 'I am a function from copied obj'; };
-    copiedObj.h = [1, 2, 3, 4];
+		expect(copiedObj).toEqual({
+			a: 1,
+			b: false,
+			c: true,
+			d: 'test changed',
+			e: 1234,
+			f: 'not null',
+			g: expect.any(Function),
+			h: [1, 2, 3, 4]
+		});
 
-    expect(copiedObj).toEqual({
-      a: 1,
-      b: false,
-      c: true,
-      d: 'test changed',
-      e: 1234,
-      f: 'not null',
-      g: expect.any(Function),
-      h: [1, 2, 3, 4]
-    });
+		expect(copiedObj.g()).toBe('I am a function from copied obj');
 
-    expect(copiedObj.g()).toBe('I am a function from copied obj');
+		expect(originalObj).toEqual({
+			a: undefined,
+			b: true,
+			c: false,
+			d: 'test',
+			e: 123,
+			f: null,
+			g: expect.any(Function),
+			h: [1, 2, 3]
+		});
 
-    expect(originalObj).toEqual({
-      a: undefined,
-      b: true,
-      c: false,
-      d: 'test',
-      e: 123,
-      f: null, 
-      g: expect.any(Function),
-      h: [1, 2, 3]
-    });
-    
-    expect(originalObj.g()).toBe('I am a function from original obj');
-
-  });
+		expect(originalObj.g()).toBe('I am a function from original obj');
+	});
 });
 
 describe('Nested Object', () => {
-  it('should create a copied object which is a hard copied version', () => {
-    
-    const user = {
-      id: 101,
-      email: 'jack@dev.com',
-      personalInfo: {
-        name: 'Jack',
-        isEmployed: false,
-        nationality: undefined,
-        address: {
-          line1: 'westwish st',
-          line2: 'washmasher',
-          city: 'wallas',
-          state: 'WX'
+	it('should create a copied object which is a hard copied version', () => {
+		const user = {
+			id: 101,
+			email: 'jack@dev.com',
+			personalInfo: {
+				name: 'Jack',
+				isEmployed: false,
+				nationality: undefined,
+				address: {
+					line1: 'westwish st',
+					line2: 'washmasher',
+					city: 'wallas',
+					state: 'WX'
+				}
+			},
+			father: {
+				id: 102,
+				email: 'daniel@dev.com',
+				personalInfo: {
+					name: 'Daniel',
+					isEmployed: true,
+					nationality: 'Australia',
+					address: {
+						line1: 'westwish loop',
+						line2: '',
+						city: 'Melbourne',
+						state: 'VIC'
+					}
+				},
+				speak: function() {
+					return "I am Jack's father and my name is Daniel.";
+				}
+			},
+			speak: function() {
+				return 'My name is Jack.';
+			}
+		};
+
+		const copiedUser = hardCopy(user);
+
+		copiedUser.personalInfo.name = 'Trump';
+		copiedUser.personalInfo.address.line1 = 'White House';
+		copiedUser.speak = function() {
+			return 'My name is Trump but I am a copied version based on Jack';
+		};
+
+		copiedUser.father.personalInfo.name = 'Tom';
+		copiedUser.father.speak = function() {
+			return 'My name is Tom but I am a copied version based on Daniel';
+		};
+		// Test properties
+		expect(user.personalInfo.name).toBe('Jack');
+		expect(user.personalInfo.address.line1).toBe('westwish st');
+
+		// Test Functions
+		expect(copiedUser.speak).toEqual(expect.any(Function));
+		expect(copiedUser.speak()).toBe(
+			'My name is Trump but I am a copied version based on Jack'
+		);
+		expect(copiedUser.father.speak()).toBe(
+			'My name is Tom but I am a copied version based on Daniel'
+		);
+		expect(user.speak()).toBe('My name is Jack.');
+	});
+});
+
+describe('Array', () => {
+	it('should create a copied array which is a hard copied version', () => {
+		const originalArray = [
+			{
+				id: 101,
+				email: 'jack@dev.com',
+				personalInfo: {
+					name: 'Jack',
+					isEmployed: false,
+					nationality: undefined,
+					address: {
+						line1: 'westwish st',
+						line2: 'washmasher',
+						city: 'wallas',
+						state: 'WX'
+					}
+				},
+				speak: function() {
+					return 'My name is Jack.';
+				}
+      },
+      {
+				id: 102,
+				email: 'daniel@dev.com',
+				personalInfo: {
+					name: 'Daniel',
+					isEmployed: true,
+					nationality: 'Australia',
+					address: {
+						line1: 'westwish loop',
+						line2: '',
+						city: 'Melbourne',
+						state: 'VIC'
+					}
+				},
+				speak: function() {
+					return "I am Jack's father and my name is Daniel.";
         }
-      },
-      family: {
-        level1: {
-          level2: {
-            level3 : 1
-          }
-        } 
-      },
-      work: function() { return 'I am working in original user obj' }
+      }
+    ];
+    
+    const copiedArray = hardCopy(originalArray);
+
+    // Change first element values in copied Array
+    copiedArray[0].personalInfo.name = 'Trump';
+		copiedArray[0].personalInfo.address.line1 = 'White House';
+		copiedArray[0].speak = function() {
+			return 'My name is Trump but I am a copied version based on Jack';
     };
-
-    const copiedUser = hardCopy(user);
-
-    copiedUser.personalInfo.name = 'Trump';
-    copiedUser.personalInfo.address.line1 = 'White House';
-    copiedUser.work = function() { return 'I am working in copied user obj'};
-
-    expect(user.personalInfo.name).toBe('Jack');
-    expect(user.personalInfo.address.line1).toBe('westwish st');
-    expect(copiedUser.work).toEqual(expect.any(Function));
-    expect(copiedUser.work()).toBe('I am working in copied user obj');
-  
+    
+    expect(originalArray[0].personalInfo.name).toBe('Jack');
+		expect(originalArray[0].personalInfo.address.line1).toBe('westwish st');
+		expect(originalArray[0].speak()).toBe('My name is Jack.');
   });
 });
